b98b19a2a5f30d491921588db6439f32e5cc1a46
error-four-o-four <jens.bachmayer@gmx.de>
2025-06-27 08:00:11 +0200

    fix: use space intendation

--- rollup.config.js
+++ rollup.config.js
@@ -6,36 +6,36 @@ import tsc from './tsconfig.json' with { type: 'json' };
 
 const input = `${tsc.compilerOptions.outDir}/index.js`
 const output = {
-	iife: pkg.jsdeliver, 
-	esm: pkg.exports['.'].import,
-	cjs: pkg.exports['.'].require,
+  iife: pkg.jsdeliver, 
+  esm: pkg.exports['.'].import,
+  cjs: pkg.exports['.'].require,
 }
 
 const plugins = [terser()]
 
 export default [
-	{
-		input,
-		output: {
-			file: output.iife,
-			name: 'BezierPath',
-			format: 'iife',
-			plugins
-		}
-	},
-	{
-		input,
-		output: [
-			{ file: output.esm.default, format: 'esm' },
-			{ file: output.cjs.default, format: 'cjs', plugins },
-		]
-	},
-	{
-		input: input.replace(/\.js$/, '.d.ts'),
-		output: [
-			{ file: output.esm.types, format: 'esm' },
-			{ file: output.cjs.types, format: 'cjs' },
-		],
-		plugins: [dts()]
-	}
+  {
+    input,
+    output: {
+      file: output.iife,
+      name: 'BezierPath',
+      format: 'iife',
+      plugins
+    }
+  },
+  {
+    input,
+    output: [
+      { file: output.esm.default, format: 'esm' },
+      { file: output.cjs.default, format: 'cjs', plugins },
+    ]
+  },
+  {
+    input: input.replace(/\.js$/, '.d.ts'),
+    output: [
+      { file: output.esm.types, format: 'esm' },
+      { file: output.cjs.types, format: 'cjs' },
+    ],
+    plugins: [dts()]
+  }
 ]
